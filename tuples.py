bc40265998e66efba825a6e9df773e661b98ab74
queen-bellah <umurerwaqueenbella@gmail.com>
2024-04-13 22:10:24 +0300

    My work on tuples

---
+++ tuples.py
@@ -0,0 +1,16 @@
+# Create a tuple
+my_tuple = (10, 20, 30, 40, 50)
+
+# Access an item by index
+first_item = my_tuple[0]
+third_item = my_tuple[2]
+slicing= my_tuple[1:4]
+new_tuple = my_tuple[:1] + my_tuple[3:]
+
+
+
+# Print the accessed items
+print("First item:", first_item)  
+print("Third item:", third_item)  
+print("Slicing items:", slicing)
+print("New tuple:", new_tuple)
